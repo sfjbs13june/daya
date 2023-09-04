9bfa29c78513b179e7c98f47c9164054b55d9800
sfjbs <daya@email.com>
2023-09-04 05:49:25 +0000

    Springboot microservice application .

---
+++ day01/simple-maven-app/Readme.md
@@ -0,0 +1,9 @@
+## springboot
+
+### build the springboot application
+    
+    mvn clean install
+
+### run the springboot application
+
+    mvn spring-boot:run
---
+++ day02/springboot-microservice-training/src/main/java/com/daya/app/SpringbootMicroserviceTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.daya.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceTrainingApplication.class, args);
+	}
+
+}
