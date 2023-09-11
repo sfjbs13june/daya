c83dcbece3ab792116bea8e5db5e049129ded3ff
sfjbs <daya@email.com>
2023-09-11 03:32:43 +0000

    hospital management

---
+++ day07/daya-hospital-management/src/main/java/com/daya/app/DayaHospitalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.daya.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayaHospitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayaHospitalManagementApplication.class, args);
+	}
+
+}
